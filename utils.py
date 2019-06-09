6afe5dbb246772f40ce6da055b74b79613e17c7a
daviscyl <daviscyl@gmail.com>
2019-06-09 07:04:49 -0400

    remove visualization codes

--- utils.py
+++ utils.py
@@ -140,9 +140,6 @@ class Lane_Finder(object):
         histogram = np.sum(
             binary_warped[binary_warped.shape[0]//2:, :], axis=0)
 
-        # Create an output image to draw on and visualize the result
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-
         # Find the peak of the left and right halves of the histogram
         # These will be the starting point for the left and right lines
         midpoint = np.int(histogram.shape[0]//2)
@@ -175,11 +172,6 @@ class Lane_Finder(object):
             win_xright_low = rightx_current - self.margin
             win_xright_high = rightx_current + self.margin
 
-            # TODO: Move to visualization notebook
-            # Draw the windows on the visualization image
-            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
-            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
-
             ### Identify the nonzero pixels in x and y within the window ###
             good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
                               (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
@@ -210,11 +202,7 @@ class Lane_Finder(object):
         rightx = nonzerox[right_lane_inds]
         righty = nonzeroy[right_lane_inds]
 
-        # Colors in the left and right lane regions
-        out_img[lefty, leftx] = [0, 0, 255]
-        out_img[righty, rightx] = [255, 0, 0]
-
-        return leftx, lefty, rightx, righty, out_img
+        return leftx, lefty, rightx, righty
 
     def find_lane_prefit(self, binary_warped):
 
@@ -238,32 +226,7 @@ class Lane_Finder(object):
         rightx = nonzerox[right_lane_inds]
         righty = nonzeroy[right_lane_inds]
 
-        # Visualization ## TODO: Move this part to visualization notebook
-        ploty = np.linspace(0, self.resolution[1]-1, self.resolution[1])
-        left_fitx = l_fit[0]*(ploty**2) + l_fit[1]*ploty + l_fit[2]
-        right_fitx = r_fit[0]*(ploty**2) + r_fit[1]*ploty + r_fit[2]
-        # Create an image to draw on and an image to show the selection window
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-        window_img = np.zeros_like(out_img)
-        # Color in left and right line pixels
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-
-        # Generate a polygon to illustrate the search window area
-        # And recast the x and y points into usable format for cv2.fillPoly()
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-self.margin, ploty]))])
-        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+self.margin, ploty])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-self.margin, ploty]))])
-        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+self.margin, ploty])))])
-        right_line_pts = np.hstack((right_line_window1, right_line_window2))
-
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
-        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
-        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-
-        return leftx, lefty, rightx, righty, result
+        return leftx, lefty, rightx, righty
 
     def sanity_check(self):
         if not (self.right_line.detected and self.left_line.detected):
